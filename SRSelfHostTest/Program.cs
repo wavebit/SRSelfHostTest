b861ac8ef81442f04f7cbed4e1fd9bd290e3f92b
DESKTOP-2OM94SB\nani <wave_bitcn@hotmail.com>
2017-07-24 09:47:47 +0800

    no message

---
+++ SRSelfHostTest/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SRSelfHostTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Set connection
+            var connection = new HubConnection($"http://127.0.0.1:8081");
+            ServicePointManager.DefaultConnectionLimit = 500;
+            //Make proxy to hub based on hub name on server
+            IHubProxy nhubProxy = connection.CreateHubProxy("Actor");
+
+            //Start connection
+
+            connection.Start().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}",
+                        task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+
+            }).Wait();
+
+            //for (int i = 0; i < 1; ++i)
+            {
+                Parallel.For(0, 8, (n) =>
+                    {
+                        var st = new Stopwatch();
+                        st.Start();
+
+                        var r = nhubProxy.Invoke<float>("Move", new object[] { 10.3f, 102.0f });
+
+                        Console.WriteLine($"Current thread {Thread.CurrentThread.ManagedThreadId}");
+                        r.Wait();
+
+                        Console.WriteLine($"{n} Elapsed {st.ElapsedMilliseconds} ms");
+                    }
+                );
+            }
+
+            connection.Stop();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+        }
+    }
+}
---
+++ SRServer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace SRServer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // This will *ONLY* bind to localhost, if you want to bind to all addresses
+            // use http://*:8080 to bind to all addresses. 
+            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
+            // for more information.
+
+            string url = "http://127.0.0.1:8081";
+
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    [HubName("Actor")]
+    public class ActorHub : Hub
+    {
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Console.WriteLine($"ConnectionId: {Context.ConnectionId} | {nameof(OnDisconnected)} ");
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnConnected()
+        {
+            Console.WriteLine($"ConnectionId: {Context.ConnectionId} | {nameof(OnConnected)} ");
+
+            return base.OnConnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            Console.WriteLine($"ConnectionId: {Context.ConnectionId} | {nameof(OnReconnected)} ");
+            return base.OnReconnected();
+        }
+
+        public float Move(float x, float y)
+        {
+            return x + y;
+        }
+    }
+}
